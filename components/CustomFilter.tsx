48db92c44deec576c615a3972054a2d89bebffbb
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-10-02 11:40:59 +0500

    changed to client-side-version

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -2,28 +2,19 @@
 
 import { useState, Fragment } from "react";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
 
 import { Listbox, Transition } from "@headlessui/react";
 import { CustomFilterProps } from "@/types";
-import { updateSearchParams } from "@/utils";
 
-const CustomFilter = ({ title, options }: CustomFilterProps) => {
-  const router = useRouter();
+const CustomFilter = ({ title, options, setFilter }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
-
-  const handleUpdateParams = (e: { title: string; value: string }) => {
-    const newPathName = updateSearchParams(title, e.value.toLowerCase());
-
-    router.push(newPathName);
-  };
 
   return (
     <div className="w-fit">
       <Listbox
         onChange={(e) => {
           setSelected(e);
-          handleUpdateParams(e);
+          setFilter(e.value);
         }}
         value={selected}
       >
